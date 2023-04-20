0fbd9176dc0536449b54aecf65a94734682dc85b
TomasBruno1 <tomas.bruno@ing.austral.edu.ar>
2023-04-20 16:33:33 +0200

    Change banner ad

--- src/components/DefaultBanner.tsx
+++ src/components/DefaultBanner.tsx
@@ -1,11 +1,11 @@
-import {banner_test_id} from '../constants';
+import {banner_id} from '../constants';
 import {BannerAd, BannerAdSize} from 'react-native-google-mobile-ads';
 import React from 'react';
 
 export const DefaultBanner = () => {
   return (
     <BannerAd
-      unitId={banner_test_id}
+      unitId={banner_id}
       size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
       onAdFailedToLoad={error => console.error(error)}
     />
